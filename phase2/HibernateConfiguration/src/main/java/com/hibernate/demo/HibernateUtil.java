a2829916d6dbf16f3e846c887113fd423c2a17a3
Shaikyusufy <97265210+Shaikyusufy@users.noreply.github.com>
2022-02-02 16:44:11 +0530

    Add files via upload

---
+++ phase2/HibernateConfiguration/src/main/java/com/hibernate/demo/HibernateUtil.java
@@ -0,0 +1,21 @@
+package com.hibernate.demo;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.product.EProduct;
+
+public class HibernateUtil {
+    public static SessionFactory beginSessionFactory() {
+    	Configuration cfg = new Configuration();
+    	cfg.configure("hibernate.cfg.xml");
+    	cfg.addAnnotatedClass(EProduct.class);
+    	
+    	
+    	//Step 2
+    	SessionFactory sf = cfg.buildSessionFactory();
+    	return sf;
+    	
+    }
+
+}
---
+++ phase2/HibernateConfiguration/src/main/java/com/hibernate/product/EProduct.java
@@ -0,0 +1,32 @@
+package com.hibernate.product;
+
+import java.math.BigDecimal;
+
+public class EProduct {
+    private long ID;
+    private String name;
+    private BigDecimal price;
+
+    
+    public EProduct() {
+            
+    }
+    public EProduct(long id, String name, BigDecimal price) {
+            this.ID = id;
+            this.name = name;
+            this.price = price;
+           
+    }
+    
+    public long getID() {return this.ID; }
+    public String getName() { return this.name;}
+    public BigDecimal getPrice() { return this.price;}
+  
+    
+    public void setID(long id) { this.ID = id;}
+    public void setName(String name) { this.name = name;}
+    public void setPrice(BigDecimal price) { this.price = price;}
+  
+    
+
+}
