4a7eeff53724017949dc9aff04c7d204a0c3df3d
Shaikyusufy <97265210+Shaikyusufy@users.noreply.github.com>
2022-02-02 16:42:27 +0530

    Add files via upload

---
+++ phase2/ConfigurationofHibernate/src/main/java/com/servelrt/deom/HibernateDemo.java
@@ -0,0 +1,62 @@
+package com.servelrt.deom;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hibernate.util.HibernateUtil;
+
+/**
+ * Servlet implementation class HibernateDemo
+ */
+@WebServlet("/HibernateDemo")
+public class HibernateDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HibernateDemo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        try {
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+
+            SessionFactory factory = HibernateUtil.getSessionFactory();
+                   
+            Session session = factory.openSession();
+            out.println("Hibernate Session opened.<br>");
+            session.close();
+            out.println("Hibernate Session closed.<br>");
+                   
+            out.println("</body></html>");
+               
+                
+            } catch (Exception ex) {
+                    throw ex;
+            }
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
